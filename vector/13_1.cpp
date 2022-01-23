c3486fcdf764fe8f6606344e392dfe1b494bea06
wavemsun <wavemsun@gmail.com>
2022-01-23 19:32:16 +0400

    var.1

--- vector/13_1.cpp
+++ vector/13_1.cpp
@@ -1,6 +1,39 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+std::vector<int> deletElement (std::vector<int> vec, int k) 
+{
+    for(int i = 0; i < vec.size(); ++i) 
+    {
+        std::cin >> vec[i];
+    }
+    return vec;
+}
 
 int main() {
-    std::vector<int> vec;
+    int n, k;
+    std::cout << "Enter number of numers: ";
+    std::cin >> n;
+    std::vector<int> vec(n);
+    //std::cout << std::endl;
+    for(int i = 0; i < vec.size(); ++i) 
+    {
+        std::cin >> vec[i];
+    }
+    for(int i = 0; i < vec.size(); ++i) 
+    {
+        std::cout << vec[i] << "  ";
+    }
+    
+    return  0;
 }
+
+
+// Задача 1. 
+ 
+// Пользователь вводит с клавиатуры число n, далее -- n целых чисел, которые нужно записать в вектор.
+// Потом пользователь вводит ещё одно значение. Нужно удалить из вектора все элементы, которые равны 
+// данному значению, и вывести итоговое состояние вектора
+
+// Пример:
+// Пользователь ввёл 5, потом числа 1, 3, 3, 5, 1. И потом -- число 3. Необходимо, чтобы в векторе остались 
+// числа 1, 5, 1, которые надо вывести их на экран. 
