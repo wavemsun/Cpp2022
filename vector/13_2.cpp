cfe5953748a3bb4de455ba9bf77be7f89d4a88dc
wavemsun <wavemsun@gmail.com>
2022-01-30 14:28:51 +0400

    Result

--- vector/13_2.cpp
+++ vector/13_2.cpp
@@ -1,34 +1,54 @@
 #include <iostream>
 #include <vector>
 
-std::vector<int> deletElement(std::vector<int> vec, int k) 
+std::vector<float> enterElement(std::vector<float> vec) 
 {
-    for(int i = 0; i < vec.size(); ++i) 
-    {
-        if (vec[i] == k)
-            vec.erase(vec.begin() + 1);
-    }
-    return vec;
-}
-
-int main() {
-    int n, k;
-    std::cout << "Enter number of elements: ";
-    std::cin >> n;
-    std::vector<int> vec(n);
-    std::cout << "Enter elements: " << std::endl;
     for(int i = 0; i < vec.size(); ++i) 
     {
         std::cin >> vec[i];
     }
-    std::cout << "Enter deleting element: ";
-    std::cin >> k;
-    std::vector<int> vecResult = deletElement(vec, k); 
-    for(int i = 0; i < vecResult.size(); ++i) 
+    return vec;
+}
+
+void printElement(std::vector<float> vec) 
+{
+    for(int i = 0; i < vec.size(); ++i) 
     {
-        std::cout << vecResult[i] << "  ";
+        std::cout << " ++ " << vec[i];
     }
     std::cout << std::endl;
+}
+
+float summPrise(std::vector<float> vecPrice, std::vector<int> vecBuy) 
+{
+    float summ = 0.0;
+    for(int i = 0; i < vecBuy.size(); ++i) 
+    {
+    summ += vecPrice[vecBuy[i]];
+    } 
+    return summ;
+}
+
+int main() {
+    std::cout << "Enter vector price:  " << std::endl;
+    std::vector<float> vecPrice = enterElement(vecPrice);
+    std::vector<float> vecBuy = enterElement(vecBuy);  
+    std::cout << std::endl;
+    printElement(vecPrice);
+    printElement(vecBuy);
+    std::cout << "+" << summPrise(vecPrice, vecBuy) << std::endl;
     
     return  0;
 }
+
+// С помощью списка инициализации задаётся вектор цен на продукты (дробными числами). Так же, 
+// с помощью списка инициализации, задаётся вектор покупок (в виде индексов), которые 
+// совершает человек.Необходимо вывести на экран итоговую стоимость покупок
+// 
+// Пример:
+// 
+// Вектор цен выглядит так:  {2.5, 4.25, 3.0, 10.0}
+// 
+// Вектор покупок: {1, 1, 0, 3}, то есть два товара по индексу 1, и по одному — индексов 0 и 3.
+// 
+// Суммарная стоимость будет равна 21.0
